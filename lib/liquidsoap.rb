d861c33fff51d5804cd174aeb8e4f5820cc9c726
Tony Miller <mcfiredrill@gmail.com>
2016-03-28 04:31:29 +0900

    remove telnet

--- lib/liquidsoap.rb
+++ lib/liquidsoap.rb
@@ -1,5 +1,4 @@
 require 'liquidsoap/socket'
-require 'liquidsoap/telnet'
 
 module Liquidsoap
   VERSION = '0.0.1'
--- spec/liquidsoap/telnet_spec.rb
+++
@@ -1,8 +0,0 @@
-require './lib/liquidsoap/telnet'
-
-describe Liquidsoap::Telnet do
-  it "connects to liquidsoap" do
-    l = Liquidsoap::Telnet.new
-    puts l.write("icecast.metadata")
-  end
-end
